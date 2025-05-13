0f571854d1b129fe9b4016c74abd4aa95d29c609
proho <prohorovalex0013@gmail.com>
2025-05-13 21:53:07 +0400

    1) Написаны классы доступа для пользователей разного уровня.

---
+++ sections/permissions.py
@@ -0,0 +1,19 @@
+from rest_framework.permissions import BasePermission
+from django.utils.translation import gettext_lazy as _
+
+from users.models import UserRoles
+
+
+class IsModerator(BasePermission):
+    message = _('You are not a moderator')
+
+    def has_permission(self, request, view):
+        if request.user.role == UserRoles.MODERATOR:
+            return True
+        return False
+
+class IsSuperuser(BasePermission):
+    message = _('You are not a superuser')
+
+    def has_permission(self, request, view):
+        return request.user.is_superuser
