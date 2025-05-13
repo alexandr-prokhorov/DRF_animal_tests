07b25aaafa0bfbb5ebfd721c628deba6214fae1b
proho <prohorovalex0013@gmail.com>
2025-05-13 22:58:54 +0400

    1) Прописан CRUD для SectionContent

--- sections/views.py
+++ sections/views.py
@@ -35,3 +35,28 @@ class SectionDestroyAPIView(DestroyAPIView):
     queryset = Section.objects.all()
     permission_classes = (IsAuthenticated, IsAdminUser | IsModerator)
 
+class SectionContentListAPIView(ListAPIView):
+    serializer_class = SectionContentListSerializer
+    queryset = SectionContent.objects.all()
+    permission_classes = (IsAuthenticated,)
+    pagination_class = SectionContentPaginator
+
+class SectionContentCreateAPIView(CreateAPIView):
+    serializer_class = SectionContentSerializer
+    permission_classes = (IsAuthenticated, IsAdminUser | IsModerator)
+
+class SectionContentRetrieveAPIView(RetrieveAPIView):
+    serializer_class = SectionContentSerializer
+    queryset = SectionContent.objects.all()
+    permission_classes = (IsAuthenticated,)
+
+class SectionContentUpdateAPIView(UpdateAPIView):
+    serializer_class = SectionContentSerializer
+    queryset = SectionContent.objects.all()
+    permission_classes = (IsAuthenticated, IsAdminUser | IsModerator)
+
+class SectionContentDestroyAPIView(DestroyAPIView):
+    serializer_class = SectionContentSerializer
+    queryset = SectionContent.objects.all()
+    permission_classes = (IsAuthenticated, IsAdminUser | IsModerator)
+
