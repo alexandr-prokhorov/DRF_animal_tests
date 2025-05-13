9ae6538c0b85752be082f18317dafd5095727780
proho <prohorovalex0013@gmail.com>
2025-05-13 22:50:43 +0400

    1) Прописаны url's для section

---
+++ sections/urls.py
@@ -0,0 +1,30 @@
+from os import path as p
+
+from django.urls import path
+
+from rest_framework.routers import DefaultRouter
+
+from sections.apps import SectionsConfig
+
+from sections.views import SectionListAPIView, SectionCreateAPIView, SectionRetrieveAPIView, SectionDestroyAPIView, \
+    SectionUpdateAPIView
+
+app_name = SectionsConfig.name
+
+router = DefaultRouter()
+
+
+section = 'section/'
+create = 'create/'
+update = 'update/'
+delete = 'delete/'
+int_pk = '<int:pk>/'
+
+urlpatterns = [
+                  # section urlpatterns
+                  path(p.join(section), SectionListAPIView.as_view(), name='section_list'),
+                  path(p.join(section, create), SectionCreateAPIView.as_view(), name='section_create'),
+                  path(p.join(section, int_pk), SectionRetrieveAPIView.as_view(), name='section_detail'),
+                  path(p.join(section, int_pk, update), SectionUpdateAPIView.as_view(), name='section_update'),
+                  path(p.join(section, int_pk, delete), SectionDestroyAPIView.as_view(), name='section_delete'),
+              ] + router.urls
