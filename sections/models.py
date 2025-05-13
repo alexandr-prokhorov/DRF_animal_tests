a2e20c5abe56eafcbd8befe30bace138858eee39
proho <prohorovalex0013@gmail.com>
2025-05-13 21:44:21 +0400

    1) Добавлена модель разделов 2) Добавлена модель содержимого 3) Написаны нужные сериализаторы 4) Созданы и проведены миграции

--- sections/models.py
+++ sections/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from users.models import NULLABLE
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Section(models.Model):
+    title = models.CharField(max_length=150, verbose_name=_("Title"))
+    description = models.TextField(verbose_name=_("Description"), **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = _("Section")
+        verbose_name_plural = _("Sections")
+        ordering = ['id']
+
+
+class SectionContent(models.Model):
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, verbose_name=_("Section"))
+    title = models.CharField(max_length=150, verbose_name=_("Title"))
+    content = models.TextField(verbose_name=_("Content"))
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = _("Section Content")
+        verbose_name_plural = _("Section Contents")
+        ordering = ['id']
